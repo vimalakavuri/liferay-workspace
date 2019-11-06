9825d473f5ae6e4ec95caacc3b861f57089a1e8e
vimalakavuri <vimala.kavuri@teamcomputers.com>
2019-11-06 15:01:12 +0530

    first commit

---
+++ modules/Helloworld/src/main/java/Helloworld/constants/HelloworldPortletKeys.java
@@ -0,0 +1,11 @@
+package Helloworld.constants;
+
+/**
+ * @author 10608
+ */
+public class HelloworldPortletKeys {
+
+	public static final String HELLOWORLD =
+		"Helloworld_HelloworldPortlet";
+
+}
---
+++ modules/Helloworld/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="helloworld.caption"/></b>
+</p>
